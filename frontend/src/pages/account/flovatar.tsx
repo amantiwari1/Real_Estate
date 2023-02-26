e9bef60904f7f66b54cdde7ef190d6b147795a83
amantiwari1 <amantiwari78632@gmail.com>
2023-02-26 18:19:02 +0530

    feat: improve ui

--- frontend/src/pages/account/flovatar.tsx
+++ frontend/src/pages/account/flovatar.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // eslint-disable
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import { query, config } from "@onflow/fcl";
 import Layout from "~/layouts/layout";
-import { Center, Loader, Text } from "@mantine/core";
+import { Center, Loader, Text, TextInput } from "@mantine/core";
 import { useQuery } from "@tanstack/react-query";
+import { useDebouncedValue } from "@mantine/hooks";
 
 const getFlovatars = async (address: string) => {
   const cadence = `
@@ -46,11 +47,20 @@ interface flovatarProps {
 }
 
 export default function Flovatar() {
-  const address = "0x2a0eccae942667be";
+  const [address, setAddress] = useState<string>("0x2a0eccae942667be");
 
-  const { data, isLoading } = useQuery(
-    ["flovatars"],
-    async () => await getFlovatars(address)
+  const [debounced] = useDebouncedValue(address, 200);
+
+  const { data, isLoading, refetch, isError } = useQuery(
+    ["flovatars", debounced],
+    async () => {
+      try {
+        return await getFlovatars(debounced);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    {}
   );
 
   useEffect(() => {
@@ -59,16 +69,6 @@ export default function Flovatar() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  if (isLoading) {
-    return (
-      <Layout>
-        <Center h="100%">
-          <Loader />
-        </Center>
-      </Layout>
-    );
-  }
-
   return (
     <Layout>
       <div className="app">
@@ -76,26 +76,51 @@ export default function Flovatar() {
           Get a List of Flovatars Living at Known Address{" "}
         </h2>
 
+        <div className="my-5 flex justify-center space-x-5">
+          <TextInput
+            label="Search Address"
+            w={400}
+            value={address}
+            placeholder="Enter address"
+            onChange={async (e) => {
+              setAddress(e.target.value);
+              await refetch();
+            }}
+            error={isError ? "Address not found" : ""}
+          />
+        </div>
+
+        {isLoading && (
+          <Center h="100%">
+            <Loader />
+          </Center>
+        )}
+
         <div id="result" className="box">
-          <p className="my-4 text-center text-2xl text-yellow-400">
-            Account with address <span className="boxed">{address}</span> has{" "}
-            <span className="bg-indigo-500">{data?.length}</span> flovatars in
-            its storage.
-          </p>
+          {!isLoading && data && data?.length > 0 && (
+            <p className="my-4 text-center text-2xl text-yellow-400">
+              Account with address <span className="boxed">{address}</span> has{" "}
+              <span className="text-4xl font-bold text-violet-500">
+                {data?.length}
+              </span>{" "}
+              flovatars in its storage.
+            </p>
+          )}
           <div className="mx-24 my-10 grid grid-cols-3 gap-2 p-8">
-            {data?.map((flovatar) => (
-              <Center key={flovatar.id}>
-                <div>
-                  <img
-                    key={flovatar.id}
-                    src={`https://flovatar.com/api/image/${flovatar.id}`}
-                    className="my-10 h-40 cursor-pointer shadow-lg shadow-emerald-800 hover:scale-150"
-                    alt="images"
-                  />
-                  <Text align="center">Flovatar {flovatar.id}</Text>
-                </div>
-              </Center>
-            ))}
+            {!isLoading &&
+              data?.map((flovatar) => (
+                <Center key={flovatar.id}>
+                  <div>
+                    <img
+                      key={flovatar.id}
+                      src={`https://flovatar.com/api/image/${flovatar.id}`}
+                      className="my-10 h-40 cursor-pointer shadow-lg shadow-emerald-800 hover:scale-150"
+                      alt="images"
+                    />
+                    <Text align="center">Flovatar {flovatar.id}</Text>
+                  </div>
+                </Center>
+              ))}
           </div>
         </div>
       </div>
